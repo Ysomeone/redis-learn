73ca4df29872902659917c2e54d14d8385ffe143
Administrator <2442752231@qq.com>
2020-07-22 12:16:45 +0800

    sd

--- src/main/java/com/yuan/redis/toolkit/StringUtil.java
+++ src/main/java/com/yuan/redis/toolkit/StringUtil.java
@@ -89,6 +89,7 @@ public class StringUtil {
         System.out.println("branch");
         System.out.println("i am branch");
         System.out.println("i am boy ");
+        System.out.println("test");
     }
 
 }
